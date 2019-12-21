70e599a457efc5fbb6bcc52b47f32644d6b039f5
Dan Wolf <dcwolf@gmail.com>
2019-12-21 13:24:10 -0500

    demonstrate basic server listening (read in strings)

--- server/main.go
+++ server/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
 
+func main() {
+	server, err := net.Listen("tcp", ":12345")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	msg, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msg)
 }
