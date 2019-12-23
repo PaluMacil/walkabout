f8e4b8f78bed5cfaf63857cb9e1b4606bdb21456
Dan Wolf <dcwolf@gmail.com>
2019-12-22 19:52:04 -0500

    create envelope for login request

--- message/login.go
+++ message/login.go
@@ -12,6 +12,26 @@ type LoginRequest struct {
 	CharacterName string
 }
 
+func (l LoginRequest) Envelope(conn net.Conn) Envelope {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(l); err != nil {
+		log.Println("Could not encode LoginResponse")
+	}
+	messageBytes := buf.Bytes()
+	return Envelope{
+		Conn: conn,
+		Raw: Raw{
+			Header: Header{
+				ProtocolVersion: CurrentProtocolVersion,
+				MessageType:     TypeLoginResponse,
+				Length:          uint16(len(messageBytes)),
+			},
+			MessageBytes: messageBytes,
+		},
+	}
+}
+
 type LoginResponse struct {
 	EntityID  uuid.UUID
 	SessionID uuid.UUID
@@ -33,7 +53,6 @@ func (l LoginResponse) Envelope(conn net.Conn) Envelope {
 				Length:          uint16(len(messageBytes)),
 			},
 			MessageBytes: messageBytes,
-			StateBytes:   nil,
 		},
 	}
 }
